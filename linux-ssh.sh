351a4f2680f8b9678c94000cd265dd08850abfd4
krish1930 <145473904+krish1930@users.noreply.github.com>
2025-07-04 08:12:08 +0530

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -6,7 +6,7 @@ set -e
 
 # Predefined environment variables
 LINUX_USER_PASSWORD="krish"
-NGROK_AUTH_TOKEN="2zMbWk0BwmMEKuXaxYemxLxyVDN_6hrjoNum3Zj81z2usQeWC"
+NGROK_AUTH_TOKEN="2zNmhIay8edvhBgiyF1WeZuQBkz_6zdFWyP1u4aMX4bk2gALb"
 LINUX_USERNAME="krish"
 LINUX_MACHINE_NAME="krish"
 
